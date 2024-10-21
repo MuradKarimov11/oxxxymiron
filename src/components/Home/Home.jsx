144fc054af38f0cef01f68428d406c3e8f34f245
Murad Karimov <muradkarimov07@gmail.com>
2024-10-21 15:33:14 +0300

    created components ShopBanner and News

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,8 @@ import  MainBanner  from "./MainBanner";
 import  TourItems  from "../Tour/TourItems";
 import TourBanner from "./TourBanner";
 import Tracks from '../Tracks/Tracks';
+import  ShopBanner  from "./ShopBanner";
+import { News } from "../News/News";
 
 export const Home = () => (
   <main className="main">
@@ -9,5 +11,7 @@ export const Home = () => (
     <TourItems/>
     <TourBanner/>
     <Tracks/>
+    <ShopBanner/>
+    <News/>
   </main>
 )
