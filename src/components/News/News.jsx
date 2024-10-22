426b852a033eb2df0b0ba0ff35220220788d8dca
Murad Karimov <muradkarimov07@gmail.com>
2024-10-22 11:56:36 +0300

    created newsReducer.js file and modified News.jsx file

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,6 +1,105 @@
+import { useCallback, useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Swiper from "swiper";
+import { SwiperSlide } from "swiper/react";
+import ScrollAnimation from "react-animate-on-scroll";
+import { Link } from "react-router-dom";
+import { Navigation } from "swiper/modules";
+
+import {getNewsItems} from '../../reducers/newsReducer';
+import { Section } from "../Section/Section";
+import { SectionTitle } from "../Title/SectionTitle";
+import Icon from '../Icon/Icon';
+
+import "swiper/css";
+
+import { SLIDER_BUTTON_TYPES } from "../../utils/constants";
 
 export const News = () => {
-  return (
-    <div>News</div>
-  )
+
+    const {PREV, NEXT} = SLIDER_BUTTON_TYPES;
+
+    const sliderRef = useRef();
+
+    const dispatch = useDispatch();
+    const {items = [], isLoading} = useSelector(({news}) => news);
+
+    useEffect(() => {
+        dispatch(getNewsItems())
+    }, [dispatch]);
+
+    const handleButtonClick = useCallback(
+        (type) => {
+            if (!sliderRef.current) return;
+
+            const { swiper } = sliderRef.current;
+
+            type === NEXT ? swiper.slideNext() : swiper.slidePrev();
+        },
+        [NEXT]
+    );
+
+    return (
+        <Section className='news-section'>
+            <div className="container">
+                <SectionTitle text="Новости"/>
+
+                {isLoading? (
+                    'Loading'
+                ) : (
+                    <Swiper
+                        ref={sliderRef}
+                        spaceBetween={24}
+                        slidesPerView={4}
+                        className="news"
+                        navigation
+                        modules={[Navigation]}
+                        breakpoints={{
+                            1366: {
+                                slidesPerView: 4,
+                            },
+                            720: {
+                                slidesPerView: 3,
+                            },
+                            360: {
+                                slidesPerView: 2,
+                            },
+                        }}
+                    >
+                        {items.map(({ title, sys: { id }, cover: { url } }, i) => (
+                            <SwiperSlide key={id}>
+                                <ScrollAnimation
+                                    animateIn="fadeInLeft"
+                                    animateOut="fadeOutLeft"
+                                    delay={i * 100}
+                                >
+                                    <Link className="news-item" to={`/news/${id}`}>
+                                        <div className="news-item__img">
+                                            <img src={url} alt={title} />
+                                        </div>
+                                        <h3 className="news-item__title">{title}</h3>
+                                    </Link>
+                                </ScrollAnimation>
+                            </SwiperSlide>
+                        ))}
+                        <div className="navigation">
+                            <div
+                                className="navigation-button navigation-prev"
+                                onClick={() => handleButtonClick(PREV)}
+                            >
+                                <Icon name="slider-arrow" />
+                            </div>
+                            <div
+                                className="navigation-button navigation-next"
+                                onClick={() => handleButtonClick(NEXT)}
+                            >
+                                <Icon name="slider-arrow" />
+                            </div>
+                        </div>
+                    </Swiper>
+                )}
+
+            </div>
+        </Section>
+    )
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -46,3 +46,8 @@ export const SOCIALS = [
     },
 ];
 
+
+export const SLIDER_BUTTON_TYPES = {
+  NEXT: "NEXT",
+  PREV: "PREV",
+};
