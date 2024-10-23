5a5fd307b51e64f4b7e9fa9ff4edf13528735ee7
Murad Karimov <muradkarimov07@gmail.com>
2024-10-23 23:56:11 +0300

    created new component Hamburger

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
 import ScrollAnimation from "react-animate-on-scroll";
-import { MENU } from "../../utils/constants";
+
 import { Logo } from "../Logo/Logo";
 import { NavLink } from "react-router-dom";
 import { Socials } from "../Socials/Socials";
+import { Hamburger } from "./Hamburger";
+
+import { MENU } from "../../utils/constants";
 
 export const Header = () => (
   <section className="header">
@@ -30,6 +33,7 @@ export const Header = () => (
           ))} 
         </nav>  
         <Socials/> 
+        <Hamburger/>
       </header>
     </div>
   </section>
